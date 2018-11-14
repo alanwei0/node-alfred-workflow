ea2754df89c5481520a611114654a2e55577d2c8
Alan <alanwei000@gmail.com>
2018-11-14 15:01:14 +0800

    lodash => _

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
     xmlbuilder: require('xmlbuilder'),
     request: require('request'),
     html: require('cheerio'),
-    lodash: require('lodash'),
+    _: require('lodash'),
 };
